ccf79cdfbed187309355b80bad33eb4a542ac3e8
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-08-28 11:15:29 +0200

    WIP

--- src/main/scala/ngsstream/Main.scala
+++ src/main/scala/ngsstream/Main.scala
@@ -1,11 +1,15 @@
 package ngsstream
 
+import java.io.File
 
+import htsjdk.samtools.fastq.{FastqReader, FastqRecord}
 import org.apache.spark._
-import org.apache.spark.streaming._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
 
-import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -13,47 +17,60 @@ object Main {
     val cmdArgs = argsParser.parse(args, Args()).getOrElse(throw new IllegalArgumentException)
 
     val conf = new SparkConf().setAppName("ngsstream").setMaster(s"local[*]")
-    val sc = new SparkContext(conf)
-    val ssc = new StreamingContext(sc, Seconds(5))
+    implicit val sc = new SparkContext(conf)
 
+    val it = new ReadFastqFiles(cmdArgs.r1, cmdArgs.r2)
 
-    val lines = ssc.socketTextStream("sasc-peter.lumcnet.prod.intern", 6666)
-    val words = lines.flatMap(_.split(" "))
-    val pairs = words.map(word => (word, 1))
-    val wordCounts = pairs.reduceByKey(_ + _)
-    wordCounts.foreachRDD { x =>
-      x.foreach(println)
-    }
+    val rdds = it.toList
 
-    ssc.start()
-
-    Thread.sleep(5000)
-
-    val future = Future {
-      import java.net._
-      import java.io._
-      import scala.io._
-
-
-      val s = new Socket(InetAddress.getByName("sasc-peter.lumcnet.prod.intern"), 6666)
-      lazy val in = new BufferedSource(s.getInputStream()).getLines()
-      val out = new PrintStream(s.getOutputStream())
-
-      out.println("Hello, world")
-      out.flush()
-      s.close()
-    }
-    Await.result(future, scala.concurrent.duration.Duration.Inf)
-    lines.stop()
-
-
-
-
-
-    Thread.sleep(10000)
+    val total = rdds.foldLeft(sc.emptyRDD[(FastqRecord, Option[FastqRecord])]) (_ ++ _)
+    println("count: " + total.count())
+    //println("records: " + total.toLocalIterator.toList.mkString)
 
     Thread.sleep(100000)
 
     sc.stop()
+  }
+
+  class ReadFastqFiles(r1: File, r2: Option[File], numberChunks: Int = 10, minSize: Int = 50000, groupSize: Int = 100)(implicit sc: SparkContext) extends Iterator[RDD[(FastqRecord, Option[FastqRecord])]] {
+    private val readerR1 = new FastqReader(r1)
+    private val readerR2 = r2.map(new FastqReader(_))
+    private val it = readerR2.map(itR2 => readerR1.iterator().zip(itR2.iterator().map(Option(_))))
+      .getOrElse(readerR1.iterator().map((_, None))).grouped(groupSize)
+
+    private val workingChunks: Array[Seq[(FastqRecord, Option[FastqRecord])]] = Array.fill(numberChunks)(Seq())
+    private val completedChunks: Array[Seq[(FastqRecord, Option[FastqRecord])]] = Array.fill(numberChunks)(Seq())
+    private val outputQueue: mutable.Queue[Seq[(FastqRecord, Option[FastqRecord])]] = mutable.Queue()
+
+    def hasNext: Boolean = it.hasNext || workingChunks.exists(_.nonEmpty) || completedChunks.exists(_.nonEmpty) || outputQueue.nonEmpty
+
+    private var currentChunkId = 0
+
+    def next(): RDD[(FastqRecord, Option[FastqRecord])] = {
+      while (outputQueue.isEmpty && it.hasNext) {
+        val list = it.next()
+        workingChunks(currentChunkId) = workingChunks(currentChunkId) ++ list
+        if (workingChunks(currentChunkId).size >= minSize) {
+          if (completedChunks(currentChunkId).size >= minSize) {
+            outputQueue += completedChunks(currentChunkId)
+            completedChunks(currentChunkId) = workingChunks(currentChunkId)
+            workingChunks(currentChunkId) = Seq()
+          }
+        }
+        currentChunkId += 1
+        if (currentChunkId >= numberChunks) currentChunkId = 0
+      }
+
+      if (outputQueue.isEmpty) {
+        (0 until numberChunks).find(i => workingChunks(i).nonEmpty || completedChunks(i).nonEmpty) match {
+          case Some(i) =>
+            outputQueue += (completedChunks(i) ++ workingChunks(i))
+            completedChunks(i) = Seq()
+            workingChunks(i) = Seq()
+          case _ => throw new IllegalStateException("No records left")
+        }
+      }
+      sc.parallelize(outputQueue.dequeue())
+    }
   }
 }
